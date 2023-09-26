34c6a90b3a39089ba928d58eb4b7e534ea34e3af
The-EleetCoder <jaijain1803@gmail.com>
2023-09-26 16:40:13 +0530

    added the functionality

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+import Spinner from "./Spinner";
 
 const Blogs = () => {
-  return (
-    <div>Blogs</div>
-  )
-}
+  // consume context
+  const { posts, loading } = useContext(AppContext);
 
-export default Blogs
+  return (
+    <div>
+      {loading ? (
+        <Spinner />
+      ) : posts.length == 0 ? (
+        <div>No Posts Available</div>
+      ) : (
+        posts.map((post) => (
+          <div key={post.id}>
+            <p className="text-xl font-bold">{post.title}</p>
+            <p>
+              By <span>{post.author}</span> on <span>{post.category}</span>
+            </p>
+            <p>Posted on {post.date}</p>
+            <p>{post.content}</p>
+            <div>
+              {post.tags.map((tag, index) => {
+                return (
+                <span key={index} > #{tag} </span>
+                )
+              })}
+            </div>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Blogs;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="">
+      <h1>The EleetCoder | Blogs</h1>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
